bdca629ef2030f87d393111b7b7f677045b5e0bb
Eunice2340 <uhofficial@yahoo.com>
2023-04-24 21:10:40 -0700

    base ten single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print base ten single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
