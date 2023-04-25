6a1c0a5036d4937c835395db5c8840a1d10fb14c
Eunice2340 <uhofficial@yahoo.com>
2023-04-24 23:10:50 -0700

    last digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#inclde <stdio.h>
+/**
+ * main - Prints combonations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n =46; n <= 56; n++)
+		for (n = 48; n <=56; n++)
+			if (m > n)
+				putchar
+
+
